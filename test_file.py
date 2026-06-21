8067c8306fd5f545ccf099058aa992be251449fd
princeimmu40-collab <princeimmu40@gmail.com>
2026-06-21 20:17:48 +0530

    Add calculate_values function with bugs
    Added a function with two bugs: ZeroDivisionError and an unused variable.

---
+++ test_file.py
@@ -0,0 +1,7 @@
+def calculate_values():
+       # బగ్ 1: ZeroDivisionError వస్తుంది
+       result = 10 / 0 
+       print(result)
+       
+       # బగ్ 2: ఈ వేరియబుల్ ఎక్కడా వాడలేదు
+       unused_val = 500
