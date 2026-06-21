3ec1b54dc27fea8e3276146b3b5067f7c8c3713e
princeimmu40-collab <princeimmu40@gmail.com>
2026-06-21 19:46:52 +0530

    Add BotTest class with ProcessData method

---
+++ buggy_code21.cs
@@ -0,0 +1,8 @@
+using System;
+public class BotTest {
+    public void ProcessData() {
+        int unassignedVariable = 555; // బాట్ ఈ అన్‌యూజ్డ్ వేరియబుల్‌ని పట్టుకోవాలి
+        string emptyStr = ""; 
+        Console.WriteLine("Testing our Gemini AI Bot Reviewer...");
+    }
+}
