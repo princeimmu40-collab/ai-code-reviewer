866457f738d7fd90b0e45264dfb999afcdd5c793
princeimmu40-collab <princeimmu40@gmail.com>
2026-06-21 19:39:46 +0530

    Add unused variable and console output for testing
    Added an unused variable and a console output for testing.

--- buggy_code.cs
+++ buggy_code.cs
@@ -1,6 +1,7 @@
 using System;
 public class DebuggerDemo {
     public void Execute() {
-        int unassignedVariable = 42; // బాట్ దీన్ని పట్టుకోవాలి
+        int unusedVariable = 999; // బాట్ ఈ అన్‌యూజ్డ్ వేరియబుల్‌ని పట్టుకోవాలి
+        Console.WriteLine("Testing AI Bot Reviewer...");
     }
 }
